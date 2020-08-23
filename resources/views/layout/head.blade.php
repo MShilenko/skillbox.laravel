ec918fa87ad52c92fbea8cd2c775fe5d4e634715
MShilenko <shilenkomp@mail.ru>
2020-08-23 19:00:47 +0300

    Downloaded (using NPM) and included standard js / css files

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -9,5 +9,6 @@
 
 	<!-- Custom styles for this template -->
 	<link href="https://fonts.googleapis.com/css?family=Playfair+Display:700,900" rel="stylesheet">
+	<link href="{{ mix('/css/app.css') }}" rel="stylesheet">
 	<link href="/css/album.css" rel="stylesheet">
 </head>
